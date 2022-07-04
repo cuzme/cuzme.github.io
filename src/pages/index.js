a739f36fae7782a2eb41bc59a3b19d4b6e2bc712
zhu.xu <zhu.xu@sunlord.com.cn>
2022-07-04 13:09:45 +0800

    add docusaurus deploy 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+// import clsx from 'clsx';
+// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import {useColorMode} from '@docusaurus/theme-common';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             Docusaurus Tutorial - 5min ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+
+function Background() {
+  const {colorMode} = useColorMode();
+  console.log(colorMode, 'Background')
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/*<div className={styles.buttons}>*/}
-        {/*  <Link*/}
-        {/*    className="button button--secondary button--lg"*/}
-        {/*    to="/docs/intro">*/}
-        {/*    Docusaurus Tutorial - 5min ⏱️*/}
-        {/*  </Link>*/}
-        {/*</div>*/}
-      </div>
-    </header>
-  );
+      <div className={styles.background} style={{backgroundImage: `url(/img/cool-background-${colorMode}.png)`}}/>
+  )
 }
 
 export default function Home() {
@@ -31,11 +42,13 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      description="Yobi's website"
+    >
+      {/*<HomepageHeader />*/}
+      {/*<main>*/}
+      {/*  <HomepageFeatures />*/}
+      {/*</main>*/}
+      <Background />
     </Layout>
   );
 }
