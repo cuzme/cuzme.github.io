d805a54df990dd68d60e6b446f7af3bc68cb5de6
zhu.xu <zhu.xu@sunlord.com.cn>
2022-07-09 21:39:12 +0800

    change githubHost

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,9 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
+  githubHost: 'github',
   organizationName: 'cuzme', // Usually your GitHub org/user name.
   projectName: 'cuzme.github.io', // Usually your repo name.
-  deploymentBranch: 'deploy',
+  deploymentBranch: 'publish',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
